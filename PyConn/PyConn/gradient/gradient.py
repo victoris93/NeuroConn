ecc9d15dac596e1e4d75145b460fef9a1fa15230
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-04-27 17:02:10 +0200

    works on the cluster

--- PyConn/PyConn/gradient/gradient.py
+++ PyConn/PyConn/gradient/gradient.py
@@ -5,7 +5,6 @@ from brainspace.gradient.alignment import ProcrustesAlignment
 from brainspace.utils.parcellation import map_to_labels
 from brainspace.datasets import load_parcellation
 from ..preprocessing.preprocessing import FmriPreppedDataSet
-
 import sys
 import os
 
@@ -25,7 +24,6 @@ def align_gradients(gradients, custom_ref = None, *args):
     aligned_gradients = np.array(Alignment.fit(gradients, ref_gradients.T).aligned_)
     return aligned_gradients
     
-
 def get_gradients(data_path, subject, n_components, task, parcellation = 'schaefer', n_parcels = 1000, kernel = 'cosine', approach = 'pca', from_mat = True, aligned = True, save = True, save_to = None):
     gm = GradientMaps(n_components = n_components, kernel = kernel, approach = approach)
     fmriprepped_data = FmriPreppedDataSet(data_path)
