62b680872ec81a3bce1c21d0a7ab6346726b7ced
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2023-05-22 14:06:24 +0200

    tests updated

--- tests/preprocessing/test_preprocessing.py
+++ tests/preprocessing/test_preprocessing.py
@@ -13,25 +13,28 @@ def test_subjects_attr():
 def test_subject():
     raw_data = RawDataset(example_data)
     subjects = raw_data.subjects
-    assert "17017" in subjects, "The subject is not on the list"
+    assert "52" in subjects, "The subject is not on the list"
 
 def test_session_names():
+    subject = '52'
     fmriprepped_data = FmriPreppedDataSet(example_data)
-    session_names_sub1 = fmriprepped_data.get_sessions(fmriprepped_data.subjects[0])
+    session_names_sub1 = fmriprepped_data.get_sessions(subject)
     assert session_names_sub1 == []
 
 def test_clean_signal_shape():
+    subject = '52'
     n_parcels = 1000
     fmriprepped_data = FmriPreppedDataSet(example_data)
-    clean_ts_= np.asarray(fmriprepped_data.clean_signal(subject = fmriprepped_data.subjects[0], task = "rest"))
+    clean_ts_= np.asarray(fmriprepped_data.clean_signal(subject, task = "rest"))
 
     assert clean_ts_.shape[0] == 1, "First dimension should be 1 (n_sessions)"
     assert clean_ts_.shape[2] == n_parcels, "Third dimension should be 1000 (n_parcels)"
 
 def test_conn_matrix():
+    subject = '52'
     fmriprepped_data = FmriPreppedDataSet(example_data)
-    conn_matrix = fmriprepped_data.get_conn_matrix(subject = fmriprepped_data.subjects[0], task = "rest", save = True)
-    path_conn_matrix = os.path.join(f'{fmriprepped_data.data_path}', 'clean_data', f'sub-{fmriprepped_data.subjects[0]}', 'func', f'conn-matrix-sub-{fmriprepped_data.subjects[0]}-rest-schaefer1000.npy')
+    conn_matrix = fmriprepped_data.get_conn_matrix(subject, task = "rest", save = True)
+    path_conn_matrix = os.path.join(f'{fmriprepped_data.data_path}', 'clean_data', f'sub-{subject}', 'func', f'conn-matrix-sub-{fmriprepped_data.subjects[0]}-rest-schaefer1000.npy')
 
     assert conn_matrix.shape[0] == 1, "First dimension should be 2 (n_sessions)"
     assert conn_matrix.shape[1] == 1000, "Second dimension should be 1000 (n_parcels)"
